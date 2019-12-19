79275724aac678974131fdcb46393c62d8c7c4f2
0xsirrush <thaibao@autonomous.nyc>
2019-12-19 13:32:35 +0700

    - auto restart

--- pdex-data-collector/dockerrun.sh
+++ pdex-data-collector/dockerrun.sh
@@ -4,4 +4,4 @@ mkdir data
 
 docker rm -f pdex-data-collector
 
-docker run -d -v $PWD/data:/data --name pdex-data-collector incognitochain/incognito-analytic:pdex-data-collector
+docker run --restart=always -d -v $PWD/data:/data --name pdex-data-collector incognitochain/incognito-analytic:pdex-data-collector
