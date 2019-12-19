9a21ff7b7fc6cff2135918cd7434a61dd5e7cdcd
0xsirrush <thaibao@autonomous.nyc>
2019-12-19 13:23:52 +0700

    - git ignore

--- pdex-data-collector/build.sh
+++ pdex-data-collector/build.sh
@@ -11,3 +11,7 @@ env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w' -o pdex-data-co
 docker rmi incognitochain/incognito-analytic:pdex-data-collector
 docker build . -t incognitochain/incognito-analytic:pdex-data-collector
 docker push incognitochain/incognito-analytic:pdex-data-collector
+
+if [ -f ./pdex-data-collector ]; then
+    rm -rf ./pdex-data-collector
+fi
