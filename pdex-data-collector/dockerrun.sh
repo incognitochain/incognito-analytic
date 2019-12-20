57ec8f811f73b03e3b0a8581152adc9dd7bad2f3
0xsirrush <thaibao@autonomous.nyc>
2019-12-20 11:27:33 +0700

    - get beacon timestamp from incognito

--- pdex-data-collector/dockerrun.sh
+++ pdex-data-collector/dockerrun.sh
@@ -2,4 +2,8 @@
 
 docker rm -f pdex-data-collector
 
+docker rmi incognitochain/incognito-analytic:pdex-data-collector
+
+docker pull incognitochain/incognito-analytic:pdex-data-collector
+
 docker run --restart=always --net host -d -v $PWD/data:/data --name pdex-data-collector incognitochain/incognito-analytic:pdex-data-collector
