94119a07b51fbbfab6e610fc5b432f155a80146c
0xsirrush <thaibao@autonomous.nyc>
2019-12-24 09:26:09 +0700

    - fetch data for token

---
+++ incognito-data-collector/entities/token.go
@@ -0,0 +1,18 @@
+package entities
+
+type CustomToken struct {
+	ID                 string   `json:"ID"`
+	Name               string   `json:"Name"`
+	Symbol             string   `json:"Symbol"`
+	Image              string   `json:"Image"`
+	Amount             uint64   `json:"Amount"`
+	IsPrivacy          bool     `json:"IsPrivacy"`
+	ListTxs            []string `json:"ListTxs"`
+	CountTxs           int      `json:"CountTxs"`
+	InitiatorPublicKey string   `json:"InitiatorPublicKey"`
+	TxInfo             string   `json:"TxInfo"`
+}
+
+type ListCustomToken struct {
+	ListCustomToken []CustomToken `json:"ListCustomToken"`
+}
