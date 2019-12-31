25c09fdce8d40d5d9bf7a057409f39566f42073f
0xsirrush <thaibao@autonomous.nyc>
2019-12-31 14:11:31 +0700

    - use python 2

--- api-service/run.sh
+++ api-service/run.sh
@@ -3,7 +3,7 @@
 mkdir -p /data
 
 echo "pip install -r /app/requirements.txt"
-pip install -r /app/requirements.txt
+pip install -r /app/requirements.txt > /data/install_python_lib.txt
 
 echo "python api.py > /data/log_api_service.txt 2>/data/error_log_api_service.txt"
 python api.py > /data/log_api_service.txt 2>/data/error_log_api_service.txt
