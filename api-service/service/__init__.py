cf387c43ea0ffd14339240c9100e8bfe198bbaad
0xsirrush <thaibao@autonomous.nyc>
2019-12-31 15:31:56 +0700

    - get db connection from db

--- api-service/service/__init__.py
+++ api-service/service/__init__.py
@@ -1,1 +1,8 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@34.94.185.164:5432/pdex'
+import os
+
+postgresuser = os.getenv('postgresuser', 'postgres')
+postgrespwd = os.getenv('postgrespwd', 'postgres')
+postgreshost = os.getenv('postgreshost', '34.94.185.164')
+postgresport = os.getenv('postgresport', '5432')
+postgresdb = os.getenv('postgresdb', 'pdex')
+SQLALCHEMY_DATABASE_URI = 'postgresql://' + postgresuser + ':' + postgrespwd + '@' + postgreshost + ':' + postgresport + '/' + postgresdb + ''
