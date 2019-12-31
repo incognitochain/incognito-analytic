9e8f80d9a026daeb238a58e0fcbc951d3b9ac024
0xsirrush <thaibao@autonomous.nyc>
2019-12-31 15:35:17 +0700

    - get db connection from os

--- api-service/dockerrun.sh
+++ api-service/dockerrun.sh
@@ -5,4 +5,20 @@ docker rmi incognitochain/incognito-analytic:incognito-analytic-api-service
 
 docker pull incognitochain/incognito-analytic:incognito-analytic-api-service
 
-docker run --restart=always -p 8080:5000 -d -v $PWD/data:/data --name incognito-analytic-api-service incognitochain/incognito-analytic:incognito-analytic-api-service
+if [ -z "$postgrespwd" ]; then
+    postgrespwd="postgres";
+fi
+
+if [ -z "$postgresuser" ]; then
+    postgresuser="postgres";
+fi
+
+if [ -z "$postgreshost" ]; then
+    postgreshost="127.0.0.1";
+fi
+
+if [ -z "$postgresport" ]; then
+    postgresport="5432";
+fi
+
+docker run --restart=always -p 8080:5000 -d -v $PWD/data:/data -e postgreshost=$postgreshost -e postgresport=$postgresport -e postgresuser=$postgresuser -e postgrespwd=$postgrespwd -e postgresdb=$postgresdb --name incognito-analytic-api-service incognitochain/incognito-analytic:incognito-analytic-api-service
--- api-service/run.sh
+++ api-service/run.sh
@@ -5,5 +5,21 @@ mkdir -p /data
 echo "pip install -r /app/requirements.txt"
 pip install -r /app/requirements.txt > /data/install_python_lib.txt
 
+if [ -z "$postgrespwd" ]; then
+    postgrespwd="postgres";
+fi
+
+if [ -z "$postgresuser" ]; then
+    postgresuser="postgres";
+fi
+
+if [ -z "$postgreshost" ]; then
+    postgreshost="127.0.0.1";
+fi
+
+if [ -z "$postgresport" ]; then
+    postgresport="5432";
+fi
+
 echo "python api.py > /data/log_api_service.txt 2>/data/error_log_api_service.txt"
-python api.py > /data/log_api_service.txt 2>/data/error_log_api_service.txt
+postgreshost=$postgreshost postgresport=$postgresport postgresuser=$postgresuser postgrespwd=$postgrespwd python api.py > /data/log_api_service.txt 2>/data/error_log_api_service.txt
